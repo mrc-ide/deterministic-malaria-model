ff018eb6a690045b15e3e21049268dc6c48b907d
jhellewell14 <lshjh2@lshtm.ac.uk>
2021-02-23 11:53:57 +0000

    Fixing errors in vignettes. Also had to rebuild the package locally to get it to recognise the TBV model. I'm worried that we are including things in the src/ folder on github that we are not meant to?

--- src/registration.c
+++ src/registration.c
@@ -24,6 +24,10 @@ extern void odin_model_IVM_SMChet_rhs_desolve(void *);
 extern void odin_model_output_dde(void *);
 extern void odin_model_rhs_dde(void *);
 extern void odin_model_rhs_desolve(void *);
+extern void odin_model_TBV_initmod_desolve(void *);
+extern void odin_model_TBV_output_dde(void *);
+extern void odin_model_TBV_rhs_dde(void *);
+extern void odin_model_TBV_rhs_desolve(void *);
 
 /* .Call calls */
 extern SEXP odin_model_contents(SEXP);
@@ -54,6 +58,13 @@ extern SEXP odin_model_metadata(SEXP);
 extern SEXP odin_model_rhs_r(SEXP, SEXP, SEXP);
 extern SEXP odin_model_set_initial(SEXP, SEXP, SEXP, SEXP);
 extern SEXP odin_model_set_user(SEXP, SEXP);
+extern SEXP odin_model_TBV_contents(SEXP);
+extern SEXP odin_model_TBV_create(SEXP);
+extern SEXP odin_model_TBV_initial_conditions(SEXP, SEXP);
+extern SEXP odin_model_TBV_metadata(SEXP);
+extern SEXP odin_model_TBV_rhs_r(SEXP, SEXP, SEXP);
+extern SEXP odin_model_TBV_set_initial(SEXP, SEXP, SEXP, SEXP);
+extern SEXP odin_model_TBV_set_user(SEXP, SEXP);
 
 static const R_CMethodDef CEntries[] = {
     {"odin_model_emanators_initmod_desolve",  (DL_FUNC) &odin_model_emanators_initmod_desolve,  1},
@@ -72,6 +83,10 @@ static const R_CMethodDef CEntries[] = {
     {"odin_model_output_dde",                 (DL_FUNC) &odin_model_output_dde,                 1},
     {"odin_model_rhs_dde",                    (DL_FUNC) &odin_model_rhs_dde,                    1},
     {"odin_model_rhs_desolve",                (DL_FUNC) &odin_model_rhs_desolve,                1},
+    {"odin_model_TBV_initmod_desolve",        (DL_FUNC) &odin_model_TBV_initmod_desolve,        1},
+    {"odin_model_TBV_output_dde",             (DL_FUNC) &odin_model_TBV_output_dde,             1},
+    {"odin_model_TBV_rhs_dde",                (DL_FUNC) &odin_model_TBV_rhs_dde,                1},
+    {"odin_model_TBV_rhs_desolve",            (DL_FUNC) &odin_model_TBV_rhs_desolve,            1},
     {NULL, NULL, 0}
 };
 
@@ -104,6 +119,13 @@ static const R_CallMethodDef CallEntries[] = {
     {"odin_model_rhs_r",                         (DL_FUNC) &odin_model_rhs_r,                         3},
     {"odin_model_set_initial",                   (DL_FUNC) &odin_model_set_initial,                   4},
     {"odin_model_set_user",                      (DL_FUNC) &odin_model_set_user,                      2},
+    {"odin_model_TBV_contents",                  (DL_FUNC) &odin_model_TBV_contents,                  1},
+    {"odin_model_TBV_create",                    (DL_FUNC) &odin_model_TBV_create,                    1},
+    {"odin_model_TBV_initial_conditions",        (DL_FUNC) &odin_model_TBV_initial_conditions,        2},
+    {"odin_model_TBV_metadata",                  (DL_FUNC) &odin_model_TBV_metadata,                  1},
+    {"odin_model_TBV_rhs_r",                     (DL_FUNC) &odin_model_TBV_rhs_r,                     3},
+    {"odin_model_TBV_set_initial",               (DL_FUNC) &odin_model_TBV_set_initial,               4},
+    {"odin_model_TBV_set_user",                  (DL_FUNC) &odin_model_TBV_set_user,                  2},
     {NULL, NULL, 0}
 };
 

c8599dfb027ff7a849318f4fb743254023471923
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2023-03-02 14:46:40 +0000

    updates to plots

--- scripts/ivm_exploration.R
+++ scripts/ivm_exploration.R
@@ -3,7 +3,7 @@
 #have created an IVM model with x3 SEI compartments, depending on whether the mosquito gets IVM from human/cattle/none at all
 require(ICDMM)
 require(tidyverse)
-
+require(cowplot)
 
 #base model
 out_0 <- run_model(model = "odin_model",
@@ -78,10 +78,13 @@ out_1_df_long <- out_1_df_long %>%
                                TRUE ~ "No ivermectin treatment"))
 
 
-ggplot(out_1_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
+out_1_plot <- ggplot(out_1_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
   geom_line()+
-  facet_wrap(~fct_relevel(ivm_categ, "Susceptible", "Exposed", "Infectious"))+
-  ylim(0, 1)
+  facet_wrap(~fct_relevel(ivm_categ,"No ivermectin treatment", "Ivermectin-treated human","Ivermectin-treated cattle"))+
+  ylim(0, 1)+
+  ggtitle("No ivermectin treatment")+
+  labs(col = "Mosquito infection state")+
+  theme_minimal()
 
 #colour-code this so coloured if no ivm, cattle or human
 
@@ -125,10 +128,13 @@ out_2_df_long <- out_2_df_long %>%
                                TRUE ~ "No ivermectin treatment"))
 
 
-ggplot(out_2_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
+out_2_plot <- ggplot(out_2_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
   geom_line()+
-  facet_wrap(~ivm_categ)+
-  ylim(0, 1)
+  facet_wrap(~fct_relevel(ivm_categ,"No ivermectin treatment", "Ivermectin-treated human","Ivermectin-treated cattle"))+
+  ylim(0, 1)+
+  ggtitle("100% human IVM coverage")+
+  labs(col = "Mosquito infection state")+
+  theme_minimal()
 
 head(out_2_df_long)
 
@@ -172,8 +178,61 @@ out_3_df_long <- out_3_df_long %>%
                                grepl("ic", state_var) ~ "Ivermectin-treated cattle",
                                TRUE ~ "No ivermectin treatment"))
 
-ggplot(out_3_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
+out_3_plot <- ggplot(out_3_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
   geom_line()+
-  facet_wrap(fct_relevel(~ivm_categ("No ivermectin treatment", "Ivermectin-treated human",
-                                    "Ivermectin-treated cattle")))+
-  ylim(0, 1)
+  facet_wrap(~fct_relevel(ivm_categ,"No ivermectin treatment", "Ivermectin-treated human","Ivermectin-treated cattle"))+
+  ylim(0, 1)+
+  ggtitle("100% cattle IVM coverage")+
+  labs(col = "Mosquito infection state")+
+  theme_minimal()
+
+
+#50% coverage in each
+out_4 <- run_model(model = "mosquito_ivermectin_model",
+                   init_EIR = 100,
+                   #increasing this EIR gets rid of the dde error
+                   gamma_c = 0.5,
+                   gamma_h = 0.5, time = 730) #need to run a bit longer to get to equilibrium
+
+out_4_df <- as.data.frame(out_4) %>%
+  select(t, Sv, Ev, Iv, Svih, Evih, Ivih, Svic, Evic, Ivic) %>%
+  rowwise() %>%
+  mutate(total_mosq = sum(Sv, Ev, Iv, Svih, Evih, Ivih, Svic, Evic, Ivic))
+
+out_4_df_prop <- out_4_df %>%
+  rowwise() %>%
+  mutate(Sv= Sv/total_mosq,
+         Ev = Ev/total_mosq,
+         Iv = Iv/total_mosq,
+         Svih = Svih/total_mosq,
+         Evih = Evih/total_mosq,
+         Ivih = Ivih/total_mosq,
+         Svic = Svic/total_mosq,
+         Evic = Evic/total_mosq,
+         Ivic = Ivic/total_mosq)
+
+out_4_df_long <- gather(out_4_df_prop, state_var, prop_mosq, Sv:Ivic, factor_key=TRUE)
+
+
+s_comp <- c("Sv", "Svic", "Svih")
+e_comp <- c("Ev", "Evic", "Evih")
+i_comp <- c("Iv", "Ivic", "Ivih")
+
+out_4_df_long <- out_4_df_long %>%
+  mutate(state_categ = case_when(state_var %in% s_comp ~ "Susceptible",
+                                 state_var %in% e_comp ~ "Exposed",
+                                 state_var %in% i_comp ~ "Infectious"),
+         ivm_categ = case_when(grepl("ih", state_var) ~ "Ivermectin-treated human",
+                               grepl("ic", state_var) ~ "Ivermectin-treated cattle",
+                               TRUE ~ "No ivermectin treatment"))
+
+out_4_plot <- ggplot(out_4_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_categ)))+
+  geom_line()+
+  facet_wrap(~fct_relevel(ivm_categ,"No ivermectin treatment", "Ivermectin-treated human","Ivermectin-treated cattle"))+
+  ylim(0, 1)+
+  ggtitle("50% cattle, 50% IVM coverage")+
+  labs(col = "Mosquito infection state")+
+  theme_minimal()
+
+plot_grid(out_1_plot, out_2_plot, out_3_plot, out_4_plot)
+

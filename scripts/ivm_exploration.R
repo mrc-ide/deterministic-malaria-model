c25513bc3324fc442e4b3987416c4c58b6149ffd
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2023-03-06 14:44:17 +0000

    changing some formatting on plots

--- scripts/ivm_exploration.R
+++ scripts/ivm_exploration.R
@@ -82,7 +82,7 @@ out_1_plot <- ggplot(out_1_df_long, aes(x = t, y = prop_mosq, col = as.factor(st
   facet_wrap(~fct_relevel(ivm_categ,"No IVM", "IVM from humans","IVM from cattle"))+
   ylim(0, 1)+
   ggtitle("No ivermectin treatment")+
-  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes in compartment")+
+  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes \n in compartment")+
   theme_minimal()
 
 #colour-code this so coloured if no ivm, cattle or human
@@ -132,7 +132,7 @@ out_2_plot <- ggplot(out_2_df_long, aes(x = t, y = prop_mosq, col = as.factor(st
   facet_wrap(~fct_relevel(ivm_categ,"No IVM", "IVM from humans","IVM from cattle"))+
   ylim(0, 1)+
   ggtitle("100% human IVM coverage")+
-  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes in compartment")+
+  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes \n in compartment")+
   theme_minimal()
 
 
@@ -179,7 +179,7 @@ out_3_plot <- ggplot(out_3_df_long, aes(x = t, y = prop_mosq, col = as.factor(st
   facet_wrap(~fct_relevel(ivm_categ,"No IVM", "IVM from humans","IVM from cattle"))+
   ylim(0, 1)+
   ggtitle("100% cattle IVM coverage")+
-  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes in compartment")+
+  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes \n in compartment")+
   theme_minimal()
 
 #50% coverage in each
@@ -226,11 +226,12 @@ out_4_plot <- ggplot(out_4_df_long, aes(x = t, y = prop_mosq, col = as.factor(st
   facet_wrap(~fct_relevel(ivm_categ,"No IVM", "IVM from humans","IVM from cattle"))+
   ylim(0, 1)+
   ggtitle("50% cattle, 50% IVM coverage")+
-  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes in compartment")+
+  labs(col = "Mosquito infection state", y = "Proportion of mosquitoes \n in compartment")+
   theme_minimal()
 
 #plot showing prop of mosquitoes, for each ivm intervention, in each compartment
 ivermectin_coverage_plots <- plot_grid(out_1_plot, out_2_plot, out_3_plot, out_4_plot)
+ggsave(ivermectin_coverage_plots, file = "C:/Users/nc1115/OneDrive - Imperial College London/PhD/PhD_malaria/plots_ivm_model/odin_model/ivm_cov_plots.svg")
 ggsave(ivermectin_coverage_plots, file = "C:/Users/nc1115/OneDrive - Imperial College London/PhD/PhD_malaria/plots_ivm_model/odin_model/ivm_cov_plots.pdf",
        device = cairo_pdf,
        width = 297,
@@ -282,7 +283,7 @@ out_1_sporo_dat <- out_1_sporo %>%
 
 out_1_sporo_dat_long <- gather(out_1_sporo_dat, ivm_categ, sporo_rate, sporo_rate_no_ivm:sporo_rate_total, factor_key = TRUE)
 
-ggplot(out_1_sporo_dat_long, aes(x = t, y = sporo_rate))+
+no_ivm_SR <- ggplot(out_1_sporo_dat_long, aes(x = t, y = sporo_rate))+
   geom_line()+
   facet_wrap(~ivm_categ)+
   ggtitle("No Ivermectin")
@@ -295,10 +296,10 @@ out_2_sporo_dat <- out_2_sporo %>%
 
 out_2_sporo_dat_long <- gather(out_2_sporo_dat, ivm_categ, sporo_rate, sporo_rate_no_ivm:sporo_rate_total, factor_key = TRUE)
 
-ggplot(out_2_sporo_dat_long, aes(x = t, y = sporo_rate))+
+ivm_h_SR <- ggplot(out_2_sporo_dat_long, aes(x = t, y = sporo_rate))+
   geom_line()+
   facet_wrap(~ivm_categ)+
-  ggtitle("No Ivermectin")
+  ggtitle("Human IVM")
 
 #ivm in cattle
 out_3_sporo <- as.data.frame(out_3)
@@ -307,10 +308,105 @@ out_3_sporo_dat <- out_3_sporo %>%
 
 out_3_sporo_dat_long <- gather(out_3_sporo_dat, ivm_categ, sporo_rate, sporo_rate_no_ivm:sporo_rate_total, factor_key = TRUE)
 
-ggplot(out_3_sporo_dat_long, aes(x = t, y = sporo_rate))+
+ivm_c_SR <- ggplot(out_3_sporo_dat_long, aes(x = t, y = sporo_rate))+
   geom_line()+
   facet_wrap(~ivm_categ)+
-  ggtitle("No Ivermectin")
+  ggtitle("Cattle IVM")
+
+#visualising this differently: just need the sporozoite rate (across all mosq pops)
+
+out_1_sporo_total <- as.data.frame(out_1)
+out_1_sporo_total <- out_1_sporo_total %>%
+  select(t, sporo_rate_total, prev) %>%
+  mutate(ivm_categ = "No IVM")
+
+out_2_sporo_total <- as.data.frame(out_2)
+out_2_sporo_total <- out_2_sporo_total %>%
+  select(t, sporo_rate_total, prev) %>%
+  mutate(ivm_categ = "Human IVM")
+
+out_3_sporo_total <- as.data.frame(out_3)
+out_3_sporo_total <- out_3_sporo_total %>%
+  select(t, sporo_rate_total, prev) %>%
+  mutate(ivm_categ = "Cattle IVM")
+
+out_4_sporo_total <- as.data.frame(out_4)
+out_4_sporo_total <- out_4_sporo_total %>%
+  select(t, sporo_rate_total, prev) %>%
+  mutate(ivm_categ = "50% Cattle, 50% Human IVM")
+
+df_list <- list(out_1_sporo_total, out_2_sporo_total, out_3_sporo_total, out_4_sporo_total)
+
+sporo_total_output <- do.call(rbind, df_list)
+
+sporo_rate_plot <- ggplot(sporo_total_output, aes(x = t, y = sporo_rate_total, col = as.factor(ivm_categ)))+
+  geom_line()+
+  ylab("Sporozoite Rate")+
+  xlab("Time")+
+  labs(col = "IVM category")+
+  theme_minimal()
+ggsave(sporo_rate_plot, file = "C:/Users/nc1115/OneDrive - Imperial College London/PhD/PhD_malaria/plots_ivm_model/odin_model/sporo_rate_plot.svg")
+
+#malaria prevalence
+malaria_prev <- ggplot(sporo_total_output, aes(x = t, y = prev, col = as.factor(ivm_categ)))+
+  geom_line()+
+  ylab("Prevalence")+
+  xlab("Time")+
+  labs(col = "IVM category")+
+  theme_minimal()
+ggsave(malaria_prev, file = "C:/Users/nc1115/OneDrive - Imperial College London/PhD/PhD_malaria/plots_ivm_model/odin_model/prev_plot.svg")
 
 
-#outputting Q under different net coverages####
+#under what scenarios for Q does cattle ivm have a bigger impact####. Need Q0 < 0.5
+out_Q_no_ivm <- run_model(model = "mosquito_ivermectin_model",
+                   init_EIR = 100,
+                   #increasing this EIR gets rid of the dde error
+                   time = 730,
+                   Q0 = 0.5, gamma_h = 0, gamma_c = 0)
+out_Q_ivm_h <- run_model(model = "mosquito_ivermectin_model",
+                          init_EIR = 100,
+                          #increasing this EIR gets rid of the dde error
+                          time = 730,
+                          Q0 = 0.5, gamma_h = 1, gamma_c = 0)
+
+out_Q_ivm_c <- run_model(model = "mosquito_ivermectin_model",
+                         init_EIR = 100,
+                         #increasing this EIR gets rid of the dde error
+                         time = 730,
+                         Q0 = 0.5, gamma_h = 0, gamma_c = 1)
+
+out_Q_ivm_c_h <- run_model(model = "mosquito_ivermectin_model",
+                         init_EIR = 100,
+                         #increasing this EIR gets rid of the dde error
+                         time = 730,
+                         Q0 = 0.5, gamma_h = 0.5, gamma_c = 0.5)
+
+out_Q_no_ivm_df <- as.data.frame(out_Q_no_ivm)
+out_Q_ivm_h_df <- as.data.frame(out_Q_ivm_h)
+out_Q_ivm_c_df <- as.data.frame(out_Q_ivm_c)
+out_Q_ivm_c_h_df <- as.data.frame(out_Q_ivm_c_h)
+
+out_Q_no_ivm_df <- out_Q_no_ivm_df %>%
+  mutate(ivm_categ = "No IVM")
+
+out_Q_ivm_h_df <- out_Q_ivm_h_df %>%
+  mutate(ivm_categ = "IVM human")
+
+out_Q_ivm_c_df <- out_Q_ivm_c_df %>%
+  mutate(ivm_categ = "IVM cattle")
+
+out_Q_ivm_c_h_df <- out_Q_ivm_c_h_df %>%
+  mutate(ivm_categ = "50% Cattle, 50% Human IVM")
+
+
+df_list2 <- list(out_Q_no_ivm_df, out_Q_ivm_h_df, out_Q_ivm_c_df, out_Q_ivm_c_h_df)
+
+sporo_total_output2 <- do.call(rbind, df_list2)
+
+ggplot(sporo_total_output2, aes(x = t, y = prev, col = as.factor(ivm_categ)))+
+  geom_line()+
+  ylab("Prevalence")+
+  xlab("Time")+
+  labs(col = "IVM category")
+
+#outputting Q under different net coverages, then repeating analysis to see how cattle ivermectin will work####

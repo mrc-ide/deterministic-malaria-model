2e812218989f5a2e97de87584fc06c7f7b7763bc
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2023-04-18 14:02:25 +0100

    checking betaa output and checking total number of mosquitoes with the different larval models

--- scripts/mosq_ivm_switch.R
+++ scripts/mosq_ivm_switch.R
@@ -86,10 +86,16 @@ out_2 <- run_model(model = "odin_model_mort",
                    time = 730,
                    mort_on = 20,
                    mort_off = 30)
+
+
+
 out_2_df <- as.data.frame(out_2) %>%
-  select(t, Sv, Ev, Iv, mv) %>%
+  select(t, Sv, Ev, Iv, mv, betaa) %>%
   rowwise() %>%
-  mutate(total_mosq = sum(Sv, Ev, Iv))
+  mutate(total_mosq = mv)
+
+ggplot(out_2_df, aes(x = t, y = betaa))+
+  geom_line()
 
 out_2_df_prop <- out_2_df %>%
   rowwise() %>%
@@ -111,9 +117,36 @@ out_2_plot <- ggplot(out_2_df_long, aes(x = t, y = prop_mosq, col = as.factor(st
 
 out_2_df_num <- out_2_df
 
-out_2_df_long_num <- gather(out_2_df_num, state_var, num_mosq, Sv:mv, factor_key=TRUE)
+out_2_df_long_num <- gather(out_2_df_num, state_var, value, Sv:betaa, factor_key=TRUE)
 
-ggplot(out_2_df_long_num, aes(x = t, y = num_mosq, col = as.factor(state_var)))+
+ggplot(out_2_df_long_num, aes(x = t, y = value, col = as.factor(state_var)))+
+  geom_line()+
+  #ylim(0, 1)+
+  #xlim(0,100)+
+  theme_minimal()+
+  geom_hline(yintercept = 82.6, lty = "dashed")+
+  geom_vline(xintercept = 20, lty = "dashed")+
+  geom_vline(xintercept = 30, lty = "dashed")
+
+#using the bad betaa (beta <- mv0*mu0*theta2)
+out_3 <- run_model(model = "odin_model_mort_bad_betaa",
+                   init_EIR = 200,
+                   #increasing this EIR gets rid of the dde error
+                   time = 730,
+                   mort_on = 20,
+                   mort_off = 30)
+
+out_3_df <- as.data.frame(out_3) %>%
+  select(t, Sv, Ev, Iv, mv, betaa) %>%
+  rowwise() %>%
+  mutate(total_mosq = mv)
+
+ggplot(out_3_df, aes(x = t, y = betaa))+
+  geom_line()
+
+out_3_df_long_num <- gather(out_3_df, state_var, value, Sv:betaa, factor_key=TRUE)
+
+ggplot(out_3_df_long_num, aes(x = t, y = value, col = as.factor(state_var)))+
   geom_line()+
   #ylim(0, 1)+
   #xlim(0,100)+
@@ -123,31 +156,23 @@ ggplot(out_2_df_long_num, aes(x = t, y = num_mosq, col = as.factor(state_var)))+
   geom_vline(xintercept = 30, lty = "dashed")
 
 
-out_3 <- run_model("odin_model",
-                   init_EIR = 100,
-                   time = 365*4,
-                   num_int = 2,
-                   itn_cov = 1,
-                   ITN_IRS_on = 1)
-out_3_df <- as.data.frame(out_3) %>%
-  select(t, Sv, Ev, Iv) %>%
-  rowwise() %>%
-  mutate(total_mosq = sum(Sv, Ev, Iv))
+#now plot the Svs from odin_model_mort and odin_model_mort_bad_betaa on top of each other
 
-out_3_df_prop <- out_3_df %>%
-  rowwise() %>%
-  mutate(Sv = Sv/total_mosq,
-         Ev = Ev/total_mosq,
-         Iv = Iv/total_mosq)
+out_2_filt <- out_2_df %>%
+  mutate(model = "full larval model")
+out_3_filt <- out_3_df %>%
+  mutate(model = "no larval model")
 
-out_3_df_long <- gather(out_3_df_prop, state_var, prop_mosq, Sv:Iv, factor_key=TRUE)
+models <- rbind(out_2_filt, out_3_filt)
 
 
-out_3_plot <- ggplot(out_3_df_long, aes(x = t, y = prop_mosq, col = as.factor(state_var)))+
+#wide to long
+models_long <- gather(models, state_var, value, Sv:total_mosq, factor_key = TRUE)
+mosq <- models_long %>%
+  filter(state_var == "total_mosq") %>%
+  select(t, state_var, value, model)
+ggplot(mosq, aes(x = t, y = value, col = as.factor(model)))+
   geom_line()+
-  ylim(0, 1)+
-  #xlim(0,100)+
-  theme_minimal()+
-  geom_hline(yintercept = 0.940, lty = "dashed")+
-  geom_vline(xintercept = 1, lty = "dashed")+
-  geom_vline(xintercept = 365*3, lty = "dashed")
+  ylab("Total num mosquitoes")+
+  geom_hline(yintercept = 82.62149, linetype = "dashed")
+

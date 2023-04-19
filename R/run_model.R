be427981d39dab7cae188e4ab5a3fec7838b9a26
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2023-04-19 14:59:38 +0100

    adding the betaa without the full larval model

--- R/run_model.R
+++ R/run_model.R
@@ -51,6 +51,7 @@ run_model <- function(model = "odin_model",
     "odin_model_TBV" = odin_model_TBV,
     "mosquito_ivermectin_model" = mosquito_ivermectin_model,
     "odin_model_mort" = odin_model_mort,
+    "odin_model_mort_bad_betaa" = odin_model_mort_bad_betaa,
     #"mosquito_ivermectin_model_no_delay" = mosquito_ivermectin_model_no_delay,
     #"odin_model_no_delay" = odin_model_no_delay,
     stop(sprintf("Unknown model '%s'", model)))

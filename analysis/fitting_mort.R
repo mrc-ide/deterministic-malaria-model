7a3b898e4e0c9a2e2bc22951f1bd0cdeaeaf4ac7
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2023-05-05 12:09:16 +0100

    writing coding for fitting

--- analysis/fitting_mort.R
+++ analysis/fitting_mort.R
@@ -1,19 +1,23 @@
 #fitting a curve to the ivermectin hazards data --> ivermectin-induced mortality rates
 
 require(tidyverse)
+require(optim)
+
 hazards <- read.table("C:/Users/nc1115/Documents/github/ivRmectin/IVM_derivation/ivermectin_hazards.txt", header = TRUE)
 hazards$d300
 hazards$d400
 hazards_long <- gather(hazards, dose, hazard, d400:d300, factor_key = TRUE)
-ggplot(hazards_long, aes(x = day, y = hazard, col = as.factor(dose)))+
-  geom_point()+
-  theme_minimal()
-
-#start plotting the functions on
 
 
-f_haz <- function(x) 1*x^2*exp(-0.2*x)
+hazards_data <- hazards %>%
+  select(day, d300) %>%
+  drop_na()
+#fitting with optim
+#minimise the sum sqs for each parameter in optim
+#https://www.r-bloggers.com/2013/03/how-to-use-optim-in-r/
 
+
+#writing for two parameters
 hazards_long %>%
   filter(dose == "d300") %>%
   ggplot(aes(x = day, y = hazard))+
@@ -21,3 +25,21 @@ hazards_long %>%
   xlim(0, 30)+
   geom_function(fun = f_haz, colour = "red")
 
+fitMe <- function(params) {
+  a <- params[1]
+  c <- params[2]
+  d <- params[3]
+  t <- 0:28
+  hazard_out <- d*t^2*exp(-c*t)+a
+  error <- sum((hazards_data$d300 - hazard_out)^2)
+  return(error)
+}
+o <- optim(c(a = 0.5 , c = 0.0001, d = 0.1), fitMe)
+o$par
+
+f_haz2 <- function(x, d, c, a)d*x^2*exp(-c*x) + a
+hazards_long %>%
+  filter(dose == "d300") %>%
+  ggplot(aes(x = day, y = hazard))+
+  geom_point()+
+  geom_function(fun = f_haz2, args = list(a = o$par[1], c = o$par[2], d= o$par[3]), colour = "red")
